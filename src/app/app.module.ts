90e4369c78fdfd2db34da21cf6bafce203d3f264
Wouter Spaak <wouter@openweb.nl>
2017-01-15 13:29:01 +0100

    Added AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyATGdY2rBufWWbKZAi8COM3BdbYTLt0iiM',
+  authDomain: 'quit-smoking-70663.firebaseapp.com',
+  databaseURL: 'https://quit-smoking-70663.firebaseio.com',
+  storageBucket: 'quit-smoking-70663.appspot.com',
+  messagingSenderId: '751775985474'
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +23,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     FormsModule,
     HttpModule
